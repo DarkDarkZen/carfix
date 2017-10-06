f8e9e96ff987acf2a9ae1413a0984427fa11bcfc
Peter Kalambet <peter@technogeek.io>
2017-10-06 16:59:47 +0300

    Add zeppelin-solidity deps through yarn

---
+++ solidity/contracts/CarFixICO.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+
+contract CarFixICO is Owned, CappedCrowdsale {
+    function CarFixICO(){
+
+    }
+}
