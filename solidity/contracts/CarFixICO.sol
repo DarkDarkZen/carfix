6b9b5a0ebed147f126e56b1721ef7167fd6f97a3
Peter Kalambet <peter@technogeek.io>
2017-10-09 17:18:25 +0300

    Update contracts folder structure

--- solidity/contracts/CarFixICO.sol
+++ solidity/contracts/CarFixICO.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.0;
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 
-contract CarFixICO is Owned, CappedCrowdsale {
+contract CarFixICO is CappedCrowdsale {
     function CarFixICO(){
 
     }
